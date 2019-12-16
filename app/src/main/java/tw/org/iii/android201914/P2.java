5e5321454672042feb4d1335cd97cdc61aab9e0d
brad <brad@brad.tw>
2019-12-16 20:14:23 +0800

    11

--- app/src/main/java/tw/org/iii/android201914/P2.java
+++ app/src/main/java/tw/org/iii/android201914/P2.java
@@ -8,24 +8,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ViewFlipper;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class P2 extends Fragment {
-
-
-    public P2() {
-        // Required empty public constructor
-    }
-
+    private View mainView;
+    private ViewFlipper viewFlipper;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_p2, container, false);
+        if (mainView == null) {
+            mainView = inflater.inflate(R.layout.fragment_p2, container, false);
+            viewFlipper = mainView.findViewById(R.id.viewFlipper);
+
+        }
+        return mainView;
     }
 
 }
